f03168384c371a7699ba630da2f29c5680f40033
Jesse Higson <jesse@superrb.com>
2021-10-16 14:33:47 +0100

    More config stuff to get this baby running

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,10 @@ module.exports = {
     {
       resolve: 'gatsby-source-prismic',
       options: {
-        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+        repositoryName: process.env.PRISMIC_API_REPOSITORY_NAME,
+        accessToken: process.env.PRISMIC_API_ACCESS_TOKEN,
+        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+        linkResolver: require("./src/utils/link-resolver").linkResolver,
         schemas: {
           homepage: require('./custom_types/homepage.json'),
           navigation: require('./custom_types/navigation.json'),
