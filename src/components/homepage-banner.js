0e6a0b360186b0e63b2fb5d1ef152a865321a0cd
Jesse Higson <jesse@superrb.com>
2021-12-30 10:52:36 +0000

    tidying up homepage banner

--- src/components/homepage-banner.js
+++ src/components/homepage-banner.js
@@ -12,14 +12,16 @@ export const HomepageBanner = ({
   <section className="homepage-banner">
     <div className="homepage-banner__container container container--flex">
       <div className="homepage-banner__column homepage-banner__column--left">
-        <div className="homepage-banner__about">
-          <h2 className="homepage-banner__about-title">{aboutTitle}</h2>
-          <div className="homepage-banner__about-content">{aboutContent}</div>
-        </div>
+        <div className="homepage-banner__content">
+          <div className="homepage-banner__about">
+            <h2 className="homepage-banner__about-title">{aboutTitle}</h2>
+            <div className="homepage-banner__about-content">{aboutContent}</div>
+          </div>
 
-        <div className="homepage-banner__work">
-          <h2 className="homepage-banner__work-title">{workTitle}</h2>
-          <div className="homepage-banner__work-content">{workContent}</div>
+          <div className="homepage-banner__work">
+            <h2 className="homepage-banner__work-title">{workTitle}</h2>
+            <div className="homepage-banner__work-content">{workContent}</div>
+          </div>
         </div>
       </div>
 
