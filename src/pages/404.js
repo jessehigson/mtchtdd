dc37947aab8fe41e395abf8b767f50c9b274c399
Jesse Higson <jesse@superrb.com>
2022-02-02 20:36:23 +0000

    styling error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,14 +3,20 @@ import { Layout } from '../components/layout'
 import { Seo } from '../components/seo'
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <Seo title="Not found" />
-    <div className="container">
-      <h1>Oh no!</h1>
-      <h3>We can't seem to find the page you're looking for.</h3>
-      <br />
-    </div>
-  </Layout>
+
+    <section className="error">
+      <div className="error__container container container--flex">
+        <div className="error__column">
+          <h1 className="error__title">Oh no!</h1>
+          <h3 className="error__subtitle">
+            We can't seem to find the page you're looking for.
+          </h3>
+        </div>
+      </div>
+    </section>
+  </>
 )
 
 export default NotFoundPage
